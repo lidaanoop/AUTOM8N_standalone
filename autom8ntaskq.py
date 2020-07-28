5723fd8e10b45da23a84ce3180d7855da772861d
lidaanoop <lidaanoop@gmail.com>
2020-07-28 21:38:36 +0530

    variable passing7

--- autom8ntaskq.py
+++ autom8ntaskq.py
@@ -7,5 +7,5 @@ app = Celery('autom8ntaskq', broker='redis://localhost:6379/0', backend="redis:/
 @app.task
 def displaycelery():
     # x = subprocess.call('sudo yum -y install nginx', shell=True,)
-    x = subprocess.call('ansible-playbook autom8n.yml --extra-vars \'{"username":"username","password":"password"}\'', shell=True)
+    x = subprocess.call('ansible-playbook lida/autom8n.yml --extra-vars \'{"username":"username","password":"password"}\'', shell=True)
     return (x)
