c7596cfb2ecd0873a893fe95cf35686228a24350
lidaanoop <lidaanoop@gmail.com>
2020-08-02 13:00:40 +0530

    variables

--- autom8ntaskq.py
+++ autom8ntaskq.py
@@ -17,6 +17,6 @@ def displaycelery():
     with open("sample.json", "w") as outfile:
         outfile.write(json_object)
     # x = subprocess.call('sudo yum -y install nginx', shell=True,)
-    x = subprocess.call('ansible-playbook -i hosts autom8n.yaml --extra-vars @sample.json', shell=True)
+    x = subprocess.call('ansible-playbook -i hosts autom8n.yaml --extra-vars sample.json', shell=True)
 
     return (x)
